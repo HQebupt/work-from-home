8481d4ddc67e759421760509a02d1c52994c6681
HQebupt <qiang.huang1991@gmail.com>
2018-01-23 19:09:14 -0700

    update data.sh

--- data.sh
+++ data.sh
@@ -8,3 +8,9 @@ python test.py 57.13	27.2	24.81
 python test.py 49.28	2.96	4
 python test.py 49.18	7.52	7.39
 
+python test.py 64.9     3.41    5.18
+python test.py 41.37    12.83   -2.61
+python test.py 49.97    19.41   -0.41
+python test.py 50.28    20.82   1.69
+python test.py 58.49    10.29   4.45
+python test.py 63.17    20.49   9.86
